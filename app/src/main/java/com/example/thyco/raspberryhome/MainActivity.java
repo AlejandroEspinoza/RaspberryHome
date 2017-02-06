acb93da390ce6fa3ac5d0924cbfa78fc639f5dca
Thyco systems <Thyco@Mini-de-Thyco.lan>
2017-02-05 23:04:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/thyco/raspberryhome/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.thyco.raspberryhome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+EditText name;
+    EditText key;
+    Button login;
+    View.OnClickListener listener = new  View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),"nombre: " + name.getText().toString() + "  key: " + key.getText().toString(), Toast.LENGTH_LONG).show();
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        name = (EditText)findViewById(R.id.name);
+        key = (EditText)findViewById(R.id.key);
+        login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(listener);
+    }
+}
