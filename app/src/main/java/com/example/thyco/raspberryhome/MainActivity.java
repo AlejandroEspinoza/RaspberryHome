0b756c239b458707b6d51d2982e140e490810d76
Alejandro <lic.alejandroespinoza@hotmail.com>
2017-02-05 23:10:40 -0600

    Organizacion de codigo

--- app/src/main/java/com/example/thyco/raspberryhome/MainActivity.java
+++ app/src/main/java/com/example/thyco/raspberryhome/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-EditText name;
+    EditText name;
     EditText key;
     Button login;
     View.OnClickListener listener = new  View.OnClickListener(){
@@ -17,14 +17,16 @@ EditText name;
             Toast.makeText(getApplicationContext(),"nombre: " + name.getText().toString() + "  key: " + key.getText().toString(), Toast.LENGTH_LONG).show();
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         name = (EditText)findViewById(R.id.name);
         key = (EditText)findViewById(R.id.key);
         login = (Button) findViewById(R.id.login);
+
         login.setOnClickListener(listener);
     }
 }
